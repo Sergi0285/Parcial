ff8b5b8e38e572fe3eee5723eb75fcfa9b458060
Sergi0285 <163783993+Sergi0285@users.noreply.github.com>
2024-10-23 19:45:59 -0500

    Update api.js

--- Frontend/paginaparcial/src/api.js
+++ Frontend/paginaparcial/src/api.js
@@ -38,6 +38,7 @@ export const makePayment = async (data) => {
 
 export const getRecentRentals = async () => {
     const response = await fetch(`${API_URL}/recent_rentals`, {
+        mode: 'no-cors',
         method: 'GET'
     });
 
